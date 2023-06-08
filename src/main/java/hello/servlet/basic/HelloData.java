b56b3e9afa9ecce1f81e9bd181d4a86c975e1eb2
jungsoo_cho <tmaxchita@gmail.com>
2023-06-08 18:40:02 +0900

    20230608 at work

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,24 @@
+package hello.servlet.basic;
+
+public class HelloData {
+
+    private String username;
+
+    private int age;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
